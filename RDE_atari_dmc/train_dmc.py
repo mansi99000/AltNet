a8bf8d8464c675f7fe69ee9ac90bff103bfe4e12
mmaheshwari_umass_edu <mmaheshwari_umass_edu@login1.unity.rc.umass.edu>
2025-03-19 19:13:14 +0000

    changed file name

--- RDE_atari_dmc/train_dmc.py
+++ RDE_atari_dmc/train_dmc.py
@@ -33,7 +33,7 @@ policy_kwargs = dict()
 
 if args.RDE:
     mode = 'RDE+SAC'
-    num_agent = 2
+    num_agent = 4
     reset = True
 elif args.SR:
     mode = 'SR+SAC'
@@ -76,7 +76,7 @@ eval_callback = EvalCallback(eval_env, best_model_save_path=log_path, log_path=l
 if args.wandb:
     policy_kwargs.update(wandb=args.wandb)
     wandb.init(project="RDE_SAC", 
-               name=f"{mode}_{args.replay_ratio}_{args.seed}",
+               name=f"{mode}_rr_{args.replay_ratio}_seed_{args.seed}_num_{num_agent}",
                group=f"{args.env}",
                job_type=f"{mode}_{args.replay_ratio}_{args.action_select_coef}",
                reinit=True)
