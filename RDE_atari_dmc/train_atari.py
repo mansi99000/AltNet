a774c9977601b76f345ae19e38797a0a06cfb10b
Mansi Maheshwari <mmaheshwari_umass_edu@login4.unity.rc.umass.edu>
2025-06-10 14:06:08 +0000

    a

--- RDE_atari_dmc/train_atari.py
+++ RDE_atari_dmc/train_atari.py
@@ -6,6 +6,8 @@ from stable_baselines3.common.vec_env import VecFrameStack
 import argparse
 import wandb
 import csv
+import os
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--env", default="AlienNoFrameskip-v4")
@@ -22,12 +24,16 @@ parser.add_argument("--action_select_coef", default=50, type=int)
 parser.add_argument("--wandb", action='store_true')
 parser.add_argument("--all_reset", action='store_true')
 parser.add_argument("--entity_name", type=str)
+parser.add_argument("--job_id", type=str, default=os.getenv("SLURM_JOB_ID", "unknown"))
 
 args = parser.parse_args()
 
 set_random_seed(args.seed)
 
 policy_kwargs = dict()
+
+branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip().decode('utf-8')
+
 
 if args.RDE:
     mode = 'RDE+DQN'
@@ -74,13 +80,14 @@ eval_callback = EvalCallback(env, best_model_save_path=log_path, log_path=log_pa
 
 if args.wandb:
     policy_kwargs.update(wandb=args.wandb)
-    wandb.init(project="RDE+DQN", entity=args.entity_name,
-               name=f"{mode}_{args.replay_ratio}_{args.seed}",
+    wandb.init(project=f"{args.env}", entity=args.entity_name,
+               name=f"{args.job_id}_{mode}_rr_{args.replay_ratio}_seed_{args.seed}_num_{num_agent}_{branch}_{reset_freq}",
                group=f"{args.env}",
-               job_type=f"{mode}_{args.replay_ratio}_{args.action_select_coef}",
+               job_type=f"{mode}_{num_agent}agents_{args.replay_ratio}_{args.action_select_coef}_{reset_freq}", 
+               dir="/work/pi_bsilva_umass_edu/mmaheshwari_umass_edu/wandb", 
                reinit=True)
 
-model = DQN('CnnPolicy', env, verbose=1, buffer_size=int(args.total_timesteps),
+model = DQN('CnnPolicy', env, verbose=0, buffer_size=int(args.total_timesteps),
             learning_starts=args.learning_starts, tau=0.005,
             train_freq=(1, "step"), gradient_steps=args.replay_ratio,
             target_update_interval=1, policy_kwargs=policy_kwargs,
