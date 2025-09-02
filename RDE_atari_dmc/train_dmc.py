e3684be40a57cfd588e385c16e6c81e810b20cf1
Mansi Maheshwari <mmaheshwari_umass_edu@login4.unity.rc.umass.edu>
2025-09-02 14:19:44 +0000

    a

--- RDE_atari_dmc/train_dmc.py
+++ RDE_atari_dmc/train_dmc.py
@@ -49,7 +49,7 @@ elif args.SR:
     ps = False
 elif args.PS:
     mode = 'PS'
-    num_agent = 2
+    num_agent = 12
     reset = True
     ps = True
 else:
@@ -93,7 +93,7 @@ if args.wandb:
                name=f"{args.job_id}_{mode}_rr_{args.replay_ratio}_seed_{args.seed}_num_{num_agent}_{branch}_{reset_freq}",
                group=f"{args.env}",
                # job_type=f"{mode}_{args.replay_ratio}_50_{reset_freq}", # _rf_{reset_freq}
-               job_type=f"{mode}_{num_agent}agents_{args.replay_ratio}_buffer_{args.buffer_size}_lr_{args.learning_rate}", 
+               job_type=f"{mode}_{num_agent}agents_{args.replay_ratio}_50_{reset_freq}", 
                dir="/work/pi_bsilva_umass_edu/mmaheshwari_umass_edu/wandb", # TODO: check if this is correct
                reinit=True)
 
